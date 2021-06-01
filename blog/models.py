a3eaa81881b5abfaab2138de3a1c6224eaa5fc5b
JohnK <werise92@gmail.com>
2021-05-31 18:55:02 -0700

    Update 1.3

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
     post = models.TextField()
+
+    def __str__(self):
+        return self.title
